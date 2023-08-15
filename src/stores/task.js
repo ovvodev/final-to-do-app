b566cb2236045e473a35fa9f5bda8b9bd0d026c9
kostas <kostasp@icloud.com>
2023-08-15 20:39:35 +0200

    add create and delete task

--- src/stores/task.js
+++ src/stores/task.js
@@ -2,17 +2,44 @@ import { defineStore } from "pinia";
 import supabase from "../lib/supabase";
 import { ref } from "vue";
 
-export const useTaskStore = defineStore("taskStore", () =>{
+
+export const useTaskStore = defineStore("taskStore", () => {
     const tasks = ref([]);
 
     const fetchTasks = async () => {
         const { data, error } = await supabase
-        .from('tasks')
-        .select()
+            .from('tasks')
+            .select()
 
         if (error) console.log("Error ", error);
         else tasks.value = data;
     }
 
-    return {tasks, fetchTasks};
+    const createTask = async (userId, taskName) => {
+
+        const { data, error } = await supabase
+            .from('tasks')
+            .insert({ user_id: userId, title: taskName })
+        if (error) {
+            console.error("Error creating task:", error);
+        } else {
+            return data;
+        }
+    }
+
+    const deleteTask = async (taskId) => {
+        const { error } = await supabase
+            .from('tasks')
+            .delete()
+            .eq('id', taskId)
+        if (error) {
+            console.error("Error deleting task:", error);
+        } else {
+            tasks.value = tasks.value.filter(task => task.id !== taskId);
+
+        }
+    }
+
+
+    return { tasks, fetchTasks, createTask, deleteTask };
 })
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,10 +1,43 @@
 <script setup>
 import { useUserStore } from "@/stores/user";
-import { ref } from "vue";
-import { useRoute } from "vue-router";
+import { ref, onMounted, watch } from "vue";
 import { useTaskStore } from "@/stores/task";
+
+const taskStore = useTaskStore();
+const userStore = useUserStore();
+const userId = userStore.user.user.id;
+const name = ref("");
+const createNewTask = () => {
+  taskStore.createTask(userId, name.value);
+  name.value = ""; // Clear the input field after submitting
+};
+onMounted(() => {
+  taskStore.fetchTasks();
+});
+
+watch(name, (newTask) => {
+  taskStore.fetchTasks(newTask);
+});
+const taskId = taskStore.tasks.id;
+const deleteCurrenttask = (taskId) => {
+  taskStore.deleteTask(taskId);
+};
 </script>
 <template>
+  <p>{{ userStore.user.user.email }}</p>
   <h1>main view</h1>
+  <button @click="taskStore.fetchTasks()">Fetch task</button>
+
+  <ul>
+    <li v-for="task in taskStore.tasks" :key="task.id">
+      {{ task.title }}{{ task.is_complete }}
+      <span><button @click="deleteCurrenttask(task.id)">delete</button></span>
+    </li>
+  </ul>
+
+  <input type="text" v-model="name" /><button @click="createNewTask">
+    submit
+  </button>
+  <p>{{ name }}</p>
 </template>
 <style></style>
